9c8032c22884bd456bcbdad573d2827ad726c875
Chase Brown <Chasebrown7@icloud.com>
2022-12-14 18:08:30 -0700

    Finished buy and sell tests in mainDeployment test case

--- test/MainDeployment.t.sol
+++ test/MainDeployment.t.sol
@@ -53,21 +53,22 @@ contract MainDeploymentTesting is Utility, Test {
         // Create holders of v1 token
         createHolders();
 
-        // (1) Deploy v2 token
+        // TODO: (1) Deploy v2 token
+        // NOTE: Check dev address and router before deploying
         gogeToken_v2 = new DogeGaySon(
-            address(0x4959bCED128E6F056A6ef959D80Bd1fCB7ba7A4B),
-            address(0xe142E9FCbd9E29C4A65C4979348d76147190a05a),
+            address(0x4959bCED128E6F056A6ef959D80Bd1fCB7ba7A4B), // verify correct address
+            address(0xe142E9FCbd9E29C4A65C4979348d76147190a05a), // verify correct address
             100_000_000_000,
             address(gogeToken_v1)
         );
 
-        // (2) Disable trading on v1
+        // TODO: (2) Disable trading on v1 to false
         gogeToken_v1.setTradingIsEnabled(false, 0);
 
-        // (3) Exclude v2 on v1
+        // TODO: (3) Exclude v2 from fees on v1
         gogeToken_v1.excludeFromFees(address(gogeToken_v2), true);
 
-        // (4) Perform migration
+        // TODO: (4) Perform migration -> 6 days
         migrateActor(joe);
         migrateActor(sal);
         migrateActor(nik);
@@ -77,6 +78,10 @@ contract MainDeploymentTesting is Utility, Test {
         // Show price of v2
         price = getPrice(address(gogeToken_v2));
         emit log_named_uint("cost of 1 v2 token", price); // 0.000002119865796663
+
+        // TODO: (5) enableTrading() on v2
+        gogeToken_v2.enableTrading();
+
     }
 
     // ~~ Utility Functions ~~
@@ -130,10 +135,147 @@ contract MainDeploymentTesting is Utility, Test {
         assertEq(gogeToken_v2.balanceOf(address(actor)), bal);
     }
 
+    /// @notice Perform a buy
+    function buy_generateFees(uint256 tradeAmt) public {
+
+        IERC20(WBNB).approve(
+            address(UNIV2_ROUTER), tradeAmt
+        );
+
+        address[] memory path = new address[](2);
+
+        path[0] = WBNB;
+        path[1] = address(gogeToken_v2);
+
+        IUniswapV2Router02(UNIV2_ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            tradeAmt,
+            0,
+            path,
+            msg.sender,
+            block.timestamp + 300
+        );
+    }
+
+    /// @notice Perform a buy
+    function sell_generateFees(uint256 tradeAmt) public {
+
+        IERC20(address(gogeToken_v2)).approve(
+            address(UNIV2_ROUTER), tradeAmt
+        );
+
+        address[] memory path = new address[](2);
+
+        path[0] = address(gogeToken_v2);
+        path[1] = WBNB;
+
+        IUniswapV2Router02(UNIV2_ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            tradeAmt,
+            0,
+            path,
+            msg.sender,
+            block.timestamp + 300
+        );
+    }
+
     // ~~ Unit Tests ~~
 
     /// @notice Initial state test.
     function test_mainDeployment_init_state() public {
+        assertTrue(true);
+    }
+
+    /// @notice Tests buy post trading being enabled.
+    function test_mainDeployment_buy() public {
+        gogeToken_v2.excludeFromFees(address(this), false);
+
+        // Verify address(this) is NOT excluded from fees and grab pre balance.
+        assert(!gogeToken_v2.isExcludedFromFees(address(this)));
+        uint256 preBal = gogeToken_v2.balanceOf(address(this));
+
+        // Deposit 10 BNB
+        uint BNB_DEPOSIT = 10 ether;
+        IWETH(WBNB).deposit{value: BNB_DEPOSIT}();
+
+        // approve purchase
+        IERC20(WBNB).approve(address(UNIV2_ROUTER), 5 ether);
+
+        address[] memory path = new address[](2);
+        path[0] = WBNB;
+        path[1] = address(gogeToken_v2);
+
+        // Get Quoted amount
+        uint[] memory amounts = IUniswapV2Router01(UNIV2_ROUTER).getAmountsOut( 5 ether, path );
+
+        // Execute purchase
+        IUniswapV2Router02(UNIV2_ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            5 ether,
+            0,
+            path,
+            address(this),
+            block.timestamp + 300
+        );
+
+        // Grab post balanace and calc amount goge tokens received.
+        uint256 postBal        = gogeToken_v2.balanceOf(address(this));
+        uint256 amountReceived = (postBal - preBal);
+        uint256 taxedAmount    = amounts[1] * gogeToken_v2.totalFees() / 100; //amounts[1] * 16%
+
+        // Verify the quoted amount is the amount received and no royalties were generated.
+        assertEq(amounts[1] - taxedAmount, amountReceived);
+        assertEq(gogeToken_v2.balanceOf(address(gogeToken_v2)), taxedAmount);
+
+        // Log
+        emit log_uint(amounts[1]);
+        emit log_uint(amountReceived);
+        emit log_uint(gogeToken_v2.balanceOf(address(gogeToken_v2)));
+    }
+
+    /// @notice Tests sell post trading being enabled.
+    function test_mainDeployment_sell() public {
+        gogeToken_v2.excludeFromFees(address(this), false);
+
+        // Verify address(this) is NOT excluded from fees and grab pre balance.
+        assert(!gogeToken_v2.isExcludedFromFees(address(this)));
+        uint256 preBal = IERC20(WBNB).balanceOf(address(this));
+
+        uint256 tradeAmt = 1_000_000 ether;
+
+        // approve sell
+        IERC20(address(gogeToken_v2)).approve(address(UNIV2_ROUTER), tradeAmt);
+
+        address[] memory path = new address[](2);
+        path[0] = address(gogeToken_v2);
+        path[1] = WBNB;
+
+        // Get Quoted amount
+        uint[] memory amounts = IUniswapV2Router01(UNIV2_ROUTER).getAmountsOut( tradeAmt, path );
+
+        // Execute purchase
+        IUniswapV2Router02(UNIV2_ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            tradeAmt,
+            0,
+            path,
+            address(this),
+            block.timestamp + 300
+        );
+
+        // Grab post balanace and calc amount goge tokens received.
+        uint256 postBal        = IERC20(WBNB).balanceOf(address(this));
+        uint256 amountReceived = (postBal - preBal);
+        uint256 afterTaxAmount = amounts[1] * 84 / 100;
+
+        // Verify the quoted amount is the amount received and no royalties were generated.
+        withinDiff(afterTaxAmount, amountReceived, 10**12);
+        assertEq(IERC20(address(gogeToken_v2)).balanceOf(address(gogeToken_v2)), amounts[0] * 16 / 100);
+
+        // Log
+        emit log_named_uint("amount bnb quoted", amounts[1]);
+        emit log_named_uint("amount bnb received", amountReceived);
+        emit log_named_uint("amount royalties", IERC20(address(gogeToken_v2)).balanceOf(address(gogeToken_v2)));
+    }
+
+    /// @notice Tests migrations post trading being enabled.
+    function test_mainDeployment_migrationPostTradingEnabled() public {
         assertTrue(true);
     }
 }
